a2e334e42616b7e57fb4ace4544a5b1d174b904d
Dominican7Chick <Farhaana_Beato@aol.com>
2014-04-23 22:55:11 +0000

    Added valdation.php, edited form

--- contact.php
+++ contact.php
@@ -1,27 +1,47 @@
-  <?php $page_id = "contact";?>
-  <?php require_once('top.php');?>
-  
-  <!-- START WRAPPER -->
-  <div id="wrapper">
-  
-  <?php require_once('includes/header.php');?>
-  <?php require_once('includes/nav.php');?>
-  
-  <!-- START MAIN CONTENT -->
-  <div id="main-content">
-    <h2>Contact Us</h2>
+<?php
+	//IMPORTANT - SET EMAIL INFO HERE
+    define('DESTINATION_EMAIL' , 'Farhaana_Beato@aol.com');
+    define('MESSAGE_SUBJECT' , 'GWD 471 CMS');
+    define('REPLY_TO' , 'Farhaana_Beato@aol.com');
+    define('FROM_ADDRESS' , 'Farhaana_Beato@aol.com');
+    define('REDIRECT_URL' , 'http://farhaanabeato.com/courses/gwd_471/gwd_471_cms/index.php');
     
-    <form action="" class="php-form">
-      <input type="text" name="name" class="form-input" placeholder="Name (required)" required />
-      <input type="email" name="email" class="form-input" placeholder="Email (required)" required />
-      <input type="text" name="subject" class="form-input" placeholder="Subject (optional)" />
-      <textarea name="message" class="form-input"  placeholder="Message (required)" required></textarea>
-      <input class="form-btn" type="submit" value="Send Message" />
-    </form>
-  </div>
-  <!-- END MAIN CONTENT -->
-  
-  <?php require_once('includes/footer.php');?>
-  
-  </div>
-  <!-- END WRAPPER -->
+    require_once('validation.php');
+?>
+
+<?php $page_id = "contact";?>
+<?php require_once('top.php');?>
+
+<!-- START WRAPPER -->
+<div id="wrapper">
+    
+    <?php require_once('includes/header.php');?>
+    <?php require_once('includes/nav.php');?>
+    
+<!-- START MAIN CONTENT -->
+<div id="main-content">
+  <h2>Contact Us</h2>
+    
+  <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post" class="php-form">
+    <label for="name"></label><?php echo @$name_error; ?>
+    <input type="text" name="name" class="form-input" placeholder="Name (required)" required />
+        
+    <label for="email"></label><?php echo @$email_error; ?>
+    <input type="email" name="email" class="form-input" placeholder="Email (required)" required />
+        
+    <label for="subject"></label>
+    <input type="text" name="subject" class="form-input" placeholder="Subject (optional)" />
+        
+    <label for="message"></label><?php echo @$message_error; ?>
+    <textarea name="message" class="form-input"  placeholder="Message (required)" required></textarea>
+        
+        
+    <input class="form-btn" type="submit" value="Send Message" />
+  </form>
+</div>
+<!-- END MAIN CONTENT -->
+    
+<?php require_once('includes/footer.php');?>
+
+</div>
+<!-- END WRAPPER -->
