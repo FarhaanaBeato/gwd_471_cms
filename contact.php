6c8a3164811d59d233195a1e5e033d4e89cafe75
Dominican7Chick <Farhaana_Beato@aol.com>
2014-04-24 01:37:50 +0000

    Added code to validation.php and contact.php

--- contact.php
+++ contact.php
@@ -1,6 +1,6 @@
 <?php
 
-    if($_POST['submit'])
+    if(@$_POST['submit'])
     {
       require_once('validation.php');
       
@@ -34,18 +34,43 @@
       }
       
       print_r($errors);
+      
+      
+      $message = verifyText($message);
+	
+      //IF ANY ERRORS DON'T SEND EMAIL, ELSE SEND EMAIL
+      if(count($errors) === 0)
+      {
+        //prepare for sending email
+        $destination = 'Farhaana_Beato@aol.com';
+        $subject = 'Comments from my website';
+        $body = "$name \n $email \n $message";
+        
+        if(mail($destination, $subject, $body))
+        {
+          echo 'Your message has been sent. Below is the info you provided:';
+          
+          //TEST TO SEE IF DATA IS RECEIVED
+          echo '<h1>Thanks for providing feedback</h1>';
+          echo '<ul>';
+          echo '<li>' . $_POST['name'] . '</li>';
+          echo '<li>' . $_POST['email'] . '</li>';
+          echo '<li>' . $_POST['message'] . '</li>';
+          echo '</ul>';
+        }
+        else
+        {
+          echo 'There was an error. Your message has NOT been sent.';
+        }
+        
+      }
+      
+      
     }
     else
     {
       //echo 'View Form';
     }
-
-    //IMPORTANT - SET EMAIL INFO HERE
-/*define('DESTINATION_EMAIL' , 'Farhaana_Beato@aol.com');
-    define('MESSAGE_SUBJECT' , 'GWD 471 CMS');
-    define('REPLY_TO' , 'Farhaana_Beato@aol.com');
-    define('FROM_ADDRESS' , 'Farhaana_Beato@aol.com');
-    define('REDIRECT_URL' , 'http://farhaanabeato.com/courses/gwd_471/gwd_471_cms/index.php');*/
 ?>
 
 <?php $page_id = "contact";?>
@@ -75,7 +100,7 @@
       <label for="message"></label><?php echo @$message_error; ?>
       <textarea name="message"  placeholder="Message (required)" required></textarea>
           
-      <input type="submit" name="submit" value="Send Message" />
+      <input type="submit" name="submit" value="Send Message" class="form-btn" />
     </fieldset>
   </form>
   
