419ba41188bb322955cd3281ee4d1f6d5a04fb56
Dominican7Chick <Farhaana_Beato@aol.com>
2014-06-04 21:17:55 -0400

    Delete top.php

--- top.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html>
-<head>
-<meta charset="UTF-8">
-<title>Beato Designs</title>
-<link href="css/style.css" rel="stylesheet" type="text/css">
-</head>
-
-<body id="<?php echo $page_id;?>">
